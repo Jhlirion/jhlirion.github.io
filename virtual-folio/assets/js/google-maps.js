7f8501cd259ba0693952dda805159ff41ee736a4
jhon <jhlirion@gmail.com>
2022-11-11 13:18:23 -0500

    actualizacion datos

--- virtual-folio/assets/js/google-maps.js
+++ virtual-folio/assets/js/google-maps.js
@@ -1,6 +1,6 @@
 function initMap() {
     // Latitude and Longitude
-    var myLatLng = {lat: -6.207690, lng: 106.985270};
+    var myLatLng = {lat: -9.9295323, lng: -76.2425571};
 
     var map = new google.maps.Map(document.getElementById('google-maps'), {
         zoom: 17,
